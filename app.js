712b94eb5a20e67c9194d277205430ece63acb38
Aaron Gray <aaronsallright@gmail.com>
2014-06-30 16:32:24 -0600

    Use route and include star selector route

--- app.js
+++ app.js
@@ -4,24 +4,31 @@ var db = require('./model');
 
 var app = express();
 
-app.get('/kittens', function(req, res) {
-  // res.set('json spaces', 40);
-  db.Kitten.find(function (err, kittens) {
-    if (err) console.error(err);
-    return res.json(JSON.stringify(kittens));
+app.route('/kittens')
+  .get(function(req, res) {
+    db.Kitten.find(function (err, kittens) {
+      if (err) console.error(err);
+      return res.json(JSON.stringify(kittens));
+    })
   })
-});
+  .post(function(req, res) {
+    console.log('posted to kittnes')
+  })
 
-app.get('/kittens/:id', function(req, res) {
-  res.set('json spaces', 40);
-  db.Kitten.find({'_id': req.params.id}, function(err, kitten) {
-    if (err) console.error(err);
-    return res.json(JSON.stringify(kitten));
+app.route('/kittens/:id')
+  .get(function(req, res) {
+    db.Kitten.find({'_id': req.params.id}, function(err, kitten) {
+      if (err) console.error(err);
+      return res.json(JSON.stringify(kitten));
+    })
   })
-});
 
 app.get('/', function(req, res) {
-  res.send('Please request /kittens/ in the url. Or, ask for a kitten by ID with /kittens/:id.');
+  res.send('Please request /kittens/ or /kittens/:id in the url');
+});
+
+app.get('*', function(req, res) {
+    res.send('Please request /kittens/ or /kittens/:id in the url');
 });
 
 var server = app.listen(3000, function() {
