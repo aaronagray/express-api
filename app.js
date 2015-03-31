0e64f45f1d5765847958fec8612fc70af90d441e
Aaron Gray <aaron@hellometa.com>
2015-03-31 00:15:27 -0600

    Update example

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-var http = require('http');
-var express = require('express');
-var db = require('./model');
+var http     = require('http'),
+    express  = require('express'),
+    db       = require('./model');
 
 var app = express();
 
-app.route('/kittens')
-  .get(function(req, res) {
+app.route('/kittens').
+
+  get(function(req, res) {
     db.Kitten.find(function (err, kittens) {
       if (err) console.error(err);
-      return res.json(JSON.stringify(kittens));
+      return res.json(kittens);
     })
-  })
-  .post(function(req, res) {
+  }).
+
+  post(function(req, res) {
     console.log('posted to kittnes')
   })
 
-app.route('/kittens/:id')
-  .get(function(req, res) {
+app.route('/kittens/:id').
+
+  get(function(req, res) {
     db.Kitten.find({'_id': req.params.id}, function(err, kitten) {
       if (err) console.error(err);
-      return res.json(JSON.stringify(kitten));
+      return res.json(kitten);
     })
   })
 
@@ -31,6 +34,6 @@ app.get('*', function(req, res) {
     res.send('Please request /kittens/ or /kittens/:id in the url');
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
 });
